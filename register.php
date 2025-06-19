e8e3e0d25f5bd7ce3f93167e8d2864d4764ac30a
Mardiah Salleh <marsalleh03@gmail.com>
2025-06-19 14:05:13 +0800

    changes made

--- register.php
+++ register.php
@@ -18,7 +18,7 @@
             <option value="student">Student</option>
             <option value="lecturer">Lecturer</option>
         </select><br><br>
-        <input type="submit" name="submit" value="Register">
+         <input type="submit" name="submit" value="Register">
     </form>
 </body>
 </html>
