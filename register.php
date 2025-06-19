fd08d6ff3d790ca9ed2ed3e8d9dc5dab1e837205
Mardiah Salleh <marsalleh03@gmail.com>
2025-06-19 13:53:46 +0800

    initial commit with register.php

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Register User</title>
+</head>
+<body>
+    <h2>User Registration</h2>
+    <form action="insert.php" method="post">
+        <label>Matric:</label><br>
+        <input type="text" name="matric" required><br>
+        <label>Name:</label><br>
+        <input type="text" name="name" required><br>
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br>
+        <label>Role:</label><br>
+        <select name="role">
+            <option value="student">Student</option>
+            <option value="lecturer">Lecturer</option>
+        </select><br><br>
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
